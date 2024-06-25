50097bbb9525f1ca6ed9b39e18e945ad392ad86d
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-06-25 05:21:37 +0300

    feat: add project files

---
+++ scripts/amazon.js
@@ -0,0 +1,59 @@
+function generateHTMLForAllProducta() {
+  let html = '';
+
+  products.forEach((product) => {
+    html += `
+      <div class="product-container">
+        <div class="product-image-container">
+          <img class="product-image" src="${product.image}">
+        </div>
+
+        <div class="product-name limit-text-to-2-lines">
+          ${product.name}
+        </div>
+
+        <div class="product-rating-container">
+          <img class="product-rating-stars" src="images/ratings/rating-${product.rating.stars * 10}.png">
+          <div class="product-rating-count link-primary">
+            ${product.rating.count}
+          </div>
+        </div>
+
+        <div class="product-price">
+          $${(product.priceCents / 100).toFixed(2)}
+        </div>
+
+        <div class="product-quantity-container">
+          <select>
+            <option selected value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+          </select>
+        </div>
+
+        <div class="product-spacer"></div>
+
+        <div class="added-to-cart">
+          <img src="images/icons/checkmark.png">
+          Added
+        </div>
+
+        <button class="add-to-cart-button button-primary">
+          Add to Cart
+        </button>
+      </div>
+    `;
+  });
+
+  return html;
+}
+
+document.querySelector('.js-prodcut-grid')
+  .innerHTML = generateHTMLForAllProducta();
