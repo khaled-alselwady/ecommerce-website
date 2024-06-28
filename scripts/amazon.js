6d02be57da83920a1740591b4fe178d4221be0fe
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-06-28 07:15:28 +0300

    feat(util): add money.js to format currency with 2 decimal places
    Added a new util folder and included a money.js file. This file contains a formatCurrency function to convert price in cents to a formatted dollar string with 2 decimal places, making the code more reusable.

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,6 @@
 import { cart } from '../data/cart.js';
 import { products } from '../data/products.js'
+import { formatCurrency } from './utils/money.js';
 
 function renderProductHTML() {
   let html = '';
@@ -23,7 +24,7 @@ function renderProductHTML() {
         </div>
 
         <div class="product-price">
-          $${(product.priceCents / 100).toFixed(2)}
+          $${formatCurrency(product.priceCents)}
         </div>
 
         <div class="product-quantity-container">
