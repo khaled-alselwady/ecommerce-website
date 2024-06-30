aefdb6389896252c96a3887aed0b547023466113
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-07-01 01:05:21 +0300

    feat(tests): create automated tests for formatCurrency

---
+++ tests/moneyTest.js
@@ -0,0 +1,21 @@
+import { formatCurrency } from '../scripts/utils/money.js';
+
+function doTest(condition, testDescription) {
+  console.log(testDescription);
+
+  if (condition) {
+    console.log('passed');
+  } else {
+    console.log('failed');
+  }
+}
+
+console.log('test suite: formatCurrency');
+
+doTest(formatCurrency(2095) === '20.95', 'converts cents into dollars');
+
+doTest(formatCurrency(0) === '0.00', 'works with 0')
+
+doTest(formatCurrency(2000.5) === '20.01', 'rounds up to the nearest cent')
+
+doTest(formatCurrency(2000.4) === '20.00', 'rounds down to the nearest cent')
